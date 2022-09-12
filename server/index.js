79c35f896b3977e3607a33b065dd4c5f320d3d7e
Rayhan Farhan <robindriscol@gmail.com>
2022-09-13 05:24:24 +0600

    added mutations for client and project

--- server/index.js
+++ server/index.js
@@ -1,18 +1,18 @@
 const express = require("express");
-const colors = require('colors')
+const colors = require("colors");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
-const connectDB = require('./config/db')
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 //connect to database
-connectDB()
+connectDB();
 
 app.use(
-  '/graphql',
+  "/graphql",
   graphqlHTTP({
     schema,
     graphiql: process.env.NODE_ENV === "development",
